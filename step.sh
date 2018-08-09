da67e351e96cc17a8fe246fe75cddb6edd5d8ae8
J-Ragone <john@moquality.com>
2018-08-09 12:42:44 -0400

    fixing tput

--- step.sh
+++ step.sh
@@ -4,7 +4,7 @@
 
     BOLD=`tput bold`
     GREEN=`tput setaf 2`
-    PURPLE=`tput setaf 555`
+    PURPLE=`tput setaf 55`
     NORMAL=`tput sgr 0` # No formatting
 
     echo -e "${BOLD}${PURPLE}Logging in to your MoQuality account...${NORMAL}"
