7c2096905315dbbac7c33090894b576b1af2e479
J-Ragone <john@moquality.com>
2018-08-09 11:52:28 -0400

    Suppressing shellscript tracing in step

--- step.sh
+++ step.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-set -ex
+{
+    set -ex
 
-mq login -a $API_KEY
-mq user
-mq upload -a $APP_ID -f $APK_PATH
-mq apps
+    echo "Logging in to your MoQuality account..."
+    mq login -a $API_KEY
+    echo "`mq user`"
+    echo "Uploading your app to MoQuality..."
+    echo "`mq upload -a $APP_ID -f $APK_PATH`"
 
-echo "**Your app has been successfully uploaded to MoQuality!**"
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
-exit 0
+    echo "**Your app has been successfully uploaded to MoQuality!**"
+    #
+    # --- Exit codes:
+    # The exit code of your Step is very important. If you return
+    #  with a 0 exit code `bitrise` will register your Step as "successful".
+    # Any non zero exit code will be registered as "failed" by `bitrise`.
+    exit 0
+} 2> /dev/null
