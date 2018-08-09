1644c67af88431a7532fa4364d29062f61bca9de
J-Ragone <john@moquality.com>
2018-08-09 11:57:15 -0400

    Adding coloring to text

--- step.sh
+++ step.sh
@@ -2,13 +2,18 @@
 {
     set -ex
 
-    echo "Logging in to your MoQuality account..."
+    GREEN='\033[0;32m'
+    PURPLE = '\033[0:35m'
+    NC='\033[0m' # No Color
+
+    echo -e "${PURPLE}Logging in to your MoQuality account...${NC}"
     mq login -a $API_KEY
     echo "`mq user`"
-    echo "Uploading your app to MoQuality..."
+    echo "${PURPLE}Uploading your app to MoQuality...${NC}"
     echo "`mq upload -a $APP_ID -f $APK_PATH`"
 
-    echo "**Your app has been successfully uploaded to MoQuality!**"
+    
+    echo -e "${GREEN}Your app has been successfully uploaded to MoQuality!${NC}"
     #
     # --- Exit codes:
     # The exit code of your Step is very important. If you return
